b68b3dab99c1ca10271780d0811bcf7001a8c2d7
shailesh_tagadghar <shaileshtagadghar13@gmail.com>
2024-11-29 11:07:34 +0530

    added post model and bloc codes

--- lib/Bloc/Post/bloc/post_bloc.dart
+++ lib/Bloc/Post/bloc/post_bloc.dart
@@ -1,13 +1,23 @@
 import 'package:bloc/bloc.dart';
+// ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
+import 'package:network_connectivity_management/Bloc/Post/data/model/post_model.dart';
+import 'package:network_connectivity_management/Bloc/Post/data/repository/post_repo.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc() : super(PostInitial()) {
-    on<PostEvent>((event, emit) {
-      // TODO: implement event handler
+  final PostRepo postRepo;
+  PostBloc(this.postRepo) : super(PostInitial()) {
+    on<FetchPosts>((event, emit) async {
+      emit(PostLoading());
+      try {
+        final posts = await postRepo.fetchPost();
+        emit(PostLoaded(posts));
+      } catch (e) {
+        emit(PostError(e.toString()));
+      }
     });
   }
 }
